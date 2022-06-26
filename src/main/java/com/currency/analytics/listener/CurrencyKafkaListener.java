01b9ee1ea4cf102ba0279dc162a66551576c425f
vvyavahare <vishal.vyavahare@clover.com>
2022-06-26 14:41:41 +0530

    kafka listener changes.

--- src/main/java/com/currency/analytics/listener/CurrencyKafkaListener.java
+++ src/main/java/com/currency/analytics/listener/CurrencyKafkaListener.java
@@ -2,11 +2,13 @@ package com.currency.analytics.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class CurrencyKafkaListener {
 
-  @KafkaListener(topics = "${kafka.bootstrap.server}")
+  @KafkaListener(topics = "${kafka.topic.currency.conversion}")
   public void consume(String msg) {
     log.info("Received message:{}", msg);
   }
